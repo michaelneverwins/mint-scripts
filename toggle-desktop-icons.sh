4d631ae6ea37b4a727b0ee655591fba0024fccb5
michaelneverwins <73746653+michaelneverwins@users.noreply.github.com>
2026-05-09 22:45:24 -0400

    Use #!/bin/sh and replace == with = in script that doesn't otherwise need Bash.

--- toggle-desktop-icons.sh
+++ toggle-desktop-icons.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # The first Boolean value determines whether icons are shown on the primary
 # monitor, while the second Boolean value determines whether icons are shown on
 # non-primary monitors. Therefore:
@@ -8,7 +8,7 @@
 #     'false::false' == "No desktop icons"
 # This script just toggles between the first and last options, and I don't care
 # because I have only one monitor plugged in as of writing this comment.
-if [ "$(gsettings get org.nemo.desktop desktop-layout)" == "'false::false'" ]
+if [ "$(gsettings get org.nemo.desktop desktop-layout)" = "'false::false'" ]
 then
 	gsettings set org.nemo.desktop desktop-layout "'true::true'"
 else
